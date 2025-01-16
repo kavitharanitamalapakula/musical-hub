b00a317f74e34747c074999b489383d84d0bb0f0
kavitharanitamalapakula <kavitharaninissy@gmail.com>
2025-01-16 20:53:50 +0530

    Hub

---
+++ login.js
@@ -0,0 +1,59 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB03LJr7MJqEKnadpRMhIcjFNJdyzJTqc8",
+  authDomain: "harmonia-hub-project.firebaseapp.com",
+  databaseURL: "https://harmonia-hub-project-default-rtdb.firebaseio.com",
+  projectId: "harmonia-hub-project",
+  storageBucket: "harmonia-hub-project.firebasestorage.app",
+  messagingSenderId: "1083132333592",
+  appId: "1:1083132333592:web:0f92d3ba502ad26197bd6d",
+  measurementId: "G-NGJV06H7YF"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginButton = document.getElementById("login");
+loginButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  
+  const emailPattern = /^[a-z\d]+@(gmail|yahoo|outlook)+\.(com|in|org|co)$/;
+
+  
+  if (!emailPattern.test(email)) {
+    alert("Please enter a valid email address (e.g., user@gmail.com).");
+    return;
+  }
+
+  
+  if (!password) {
+    alert("Please enter your password.");
+    return;
+  }
+
+  
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Login successful!");
+      window.location.href = "./product.html"; 
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+
+     
+      if (errorCode === "auth/user-not-found") {
+        alert("User not found. Please register first.");
+      } else if (errorCode === "auth/wrong-password") {
+        alert("Password is incorrect. Please try again."); 
+      } else {
+        alert("An error occurred: " + error.message); 
+      }
+    });
+});
---
+++ register.js
@@ -0,0 +1,74 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB03LJr7MJqEKnadpRMhIcjFNJdyzJTqc8",
+  authDomain: "harmonia-hub-project.firebaseapp.com",
+  databaseURL: "https://harmonia-hub-project-default-rtdb.firebaseio.com",
+  projectId: "harmonia-hub-project",
+  storageBucket: "harmonia-hub-project.firebasestorage.app",
+  messagingSenderId: "1083132333592",
+  appId: "1:1083132333592:web:0f92d3ba502ad26197bd6d",
+  measurementId: "G-NGJV06H7YF"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const emailPattern = /^[a-z\d]+@(gmail|yahoo|outlook)+\.(com|in|org|co)$/;
+  const passwordPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[$%&#@]).{8,16}$/;
+
+  if (!emailPattern.test(email)) {
+    alert(
+      "Invalid email format! Please use a valid email (e.g., user@gmail.com)."
+    );
+    return;
+  }
+
+  if (!passwordPattern.test(password)) {
+    alert(
+      "Invalid password format! Password must contain at least:\n" +
+        "- 1 lowercase letter\n" +
+        "- 1 uppercase letter\n" +
+        "- 1 digit\n" +
+        "- 1 special character ($, %, &, #, @)\n" +
+        "- Length between 8-16 characters."
+    );
+    return;
+  }
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      set(ref(database, "users/" + user.uid), {
+        email: email,
+        password: password,
+      });
+
+      alert("Account created successfully!");
+      window.location.href = "./login.html";
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      alert("Error: " + errorMessage);
+    });
+});
