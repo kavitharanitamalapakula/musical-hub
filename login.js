f7668d10287765efcb97d8c3a41aa9109d85c5c7
kavitharanitamalapakula <kavitharaninissy@gmail.com>
2025-01-17 15:35:20 +0530

    Forgot Password

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB03LJr7MJqEKnadpRMhIcjFNJdyzJTqc8",
@@ -23,37 +23,58 @@ loginButton.addEventListener("click", function (event) {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  
+
   const emailPattern = /^[a-z\d]+@(gmail|yahoo|outlook)+\.(com|in|org|co)$/;
 
-  
+
   if (!emailPattern.test(email)) {
     alert("Please enter a valid email address (e.g., user@gmail.com).");
     return;
   }
 
-  
+
   if (!password) {
     alert("Please enter your password.");
     return;
   }
 
-  
+
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       alert("Login successful!");
-      window.location.href = "./product.html"; 
+      window.location.href = "./product.html";
     })
     .catch((error) => {
       const errorCode = error.code;
 
-     
+
       if (errorCode === "auth/user-not-found") {
         alert("User not found. Please register first.");
       } else if (errorCode === "auth/wrong-password") {
-        alert("Password is incorrect. Please try again."); 
+        alert("Password is incorrect. Please try again.");
       } else {
-        alert("An error occurred: " + error.message); 
+        alert("An error occurred: " + error.message);
       }
     });
 });
+// reset
+const reset = document.getElementById("reset");
+reset.addEventListener("click",function(event){
+  event.preventDefault()
+
+
+  const email = document.getElementById("email").value;
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+    // Password reset email sent!
+    // ..
+    alert("Email sent")
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    alert(errorMessage)
+  });
+
+})
